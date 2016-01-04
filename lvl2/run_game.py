f0fa88e3846c2069c7c1151713dd65e73c7e56ee
Thomas Stassin <thomas.stassin@gsmproductions.com>
2016-01-04 22:52:52 +0100

    add comment with 'T:'

--- lvl2/run_game.py
+++ lvl2/run_game.py
@@ -2,26 +2,27 @@ import world
 
 
 def main():
-
-    dungeon_size = (5, 5)
+    # T: je te laisse regarder le dungeon 6, 6 que tu puisses remarquer que le héro
+    # ne pourras pas atteindre sa cible
+    dungeon_size = (6, 6)
     game = world.Game(dungeon_size)
     game.run()
 
 
 def game_update(hero):
     # Exemple à supprimer :p
-    # if hero.is_at_goal():
-    #     print("I DID IT!")
-    # else:
-    #     if hero.is_tile_free():
-    #         hero.move()
-    #     else:
-    #         hero.turn_left()
-    #         if hero.is_tile_free():
-    #             hero.move()
-    #         else:
-    #             hero.turn_right()
-    #             hero.turn_right()
+    if hero.is_at_goal():
+        print("I DID IT!")
+    else:
+        if hero.is_tile_free():
+            hero.move()
+        else:
+            hero.turn_left()
+            if hero.is_tile_free():
+                hero.move()
+            else:
+                hero.turn_right()
+                hero.turn_right()
     pass
 
 if __name__ == '__main__':
